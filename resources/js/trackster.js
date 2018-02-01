1df821085be6fd7a97c867c0d0299727e84c59af
vrrajkum <vivek.rajkumar@protonmail.com>
2018-02-01 08:16:02 -0500

    Initial commit.

---
+++ resources/js/trackster.js
@@ -0,0 +1,17 @@
+var Trackster = {};
+
+/*
+  Given an array of track data, create the HTML for a Bootstrap row for each.
+  Append each "row" to the container in the body to display all tracks. 
+*/
+Trackster.renderTracks = function(tracks) {
+
+};
+
+/*
+  Given a search term as a string, query the LastFM API.
+  Render the tracks given in the API query response.
+*/
+Trackster.searchTracksByTitle = function(title) {
+
+};
