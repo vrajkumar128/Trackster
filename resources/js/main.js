ab0d52adf6164511db98153fed587f7a89f407a1
vrrajkum <vivek.rajkumar@protonmail.com>
2018-02-02 03:02:32 -0500

    Add and style header.

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,7 @@ var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
