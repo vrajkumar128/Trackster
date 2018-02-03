7008cfca407dc5a859d8e8761b5718f9aa357a4d
vrrajkum <vivek.rajkumar@protonmail.com>
2018-02-03 03:47:16 -0500

    Write JavaScript to call last.fm API and render the returned data.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,51 +1,99 @@
 const Trackster = {};
 const apiKey = "65f045bd924b56ad4d062aa4acbee30d";
+const $trackList = $('#songs .container-fluid');
 
-$(document).ready(function() {
-  $('#search-button').click(function() {
-    Trackster.searchTracksByTitle($('#search-box').val());
+$(document).ready(() => {
+
+  // Pass the search box's input into .searchTracksByTitle() and log the returned responses
+  let search = async function() {
+    let searchInput = $('#search-box').val();
+    if (searchInput.length > 0) {
+      let responses = await Trackster.searchTracksByTitle(searchInput);
+      console.log(responses);
+    }
+  };
+
+  // Execute search when search button is clicked
+  $('#search-button').click(search);
+
+  // Click search button when user presses Enter with input field focused
+  $("#search-box").keypress(e => {
+    if (e.which === 13) {
+      $('#search-button').click();
+    }
   });
+
 });
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks.
+  Append each row to the container in the body to display all tracks.
 */
 
-Trackster.renderTracks = function(tracks) {
-  tracks.forEach(function() {
+Trackster.renderTracks = response => {
+  if (response.track && response.track.url && response.track.name && response.track.artist.name && response.track.album && response.track.listeners && response.track.duration) {
     const trackHtml =
       '<div class="row d-flex align-items-center">' +
-        '<div class="col-1">' +
-          '<a href="' + track.url
-
-
-  });
+      '  <div class="col-1">' +
+      '    <a href="' + response.track.url + '" target="_blank">' +
+      '      <i class="far fa-play-circle fa-2x"></i>' +
+      '    </a>' +
+      '  </div>' +
+      '  <span class="col-3">' + response.track.name + '</span>' +
+      '  <span class="col-2">' + response.track.artist.name + '</span>' +
+      '  <span class="col-3">' + response.track.album.title + '</span>' +
+      '  <figure class="col-1 mt-3">' +
+      '    <img class="w-100" src="' + response.track.album.image[0]["#text"] + '" alt="Album art" />' +
+      '  </figure>' +
+      '  <span class="col-1">' + response.track.listeners + '</span>' +
+      '  <span class="col-1">' + response.track.duration + '</span>' +
+      '</div>';
+    $trackList.append(trackHtml);
+  }
 };
 
 /*
   Given a search term as a string, query the LastFM API.
-  Render the tracks given in the API query response.
+  Render the data returned by the API query.
 */
 
-Trackster.searchTracksByTitle = async (title) => {
-  const responses = [];
+Trackster.searchTracksByTitle = async title => {
+  $trackList.empty();
+  let responses = [];
 
   try {
-    const response1 = await fetch(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=${title}&api_key=${apiKey}&format=json`)
+    let response1 = await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${title}&api_key=${apiKey}&format=json`);
     if (response1.ok) {
-      responses.push(response1);
-    }
-  }
-  catch (error) {
-    console.log(error);
-  }
+      let jsonResponse1 = await response1.json();
+      let artists = [];
+      let titles = [];
+      jsonResponse1.results.trackmatches.track.forEach(track => {
+        artists.push(track.artist);
+        titles.push(track.name);
+      });
 
-  try {
-    const response2 = await fetch(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&track=${title}&api_key=${apiKey}&format=json`)
-    if (response2.ok) {
-      responses.push(response2);
+      async function getInfo() {
+        for (let arrIndex = 0; arrIndex < artists.length; arrIndex++) {
+          try {
+            let response2 = await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getInfo&artist=${artists[arrIndex]}&track=${titles[arrIndex]}&api_key=${apiKey}&format=json`);
+            if (response2.ok) {
+              let jsonResponse2 = await response2.json();
+              responses.push(jsonResponse2);
+              Trackster.renderTracks(jsonResponse2);
+            } else {
+              throw new Error("Request Two Failed!");
+            }
+          }
+          catch (error) {
+            console.log(error);
+          }
+        }
+      }
+      await getInfo();
+    } else {
+      throw new Error("Request One Failed!");
     }
+    return responses;
   }
   catch (error) {
     console.log(error);
